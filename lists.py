eecd0d717958831a579664a5f1f93f4257c3e30c
brandoo419 <brandonkiprotich68@gmail.com>
2026-07-07 11:41:56 +0300

    Complete Day 5 -Python Lists

---
+++ lists.py
@@ -0,0 +1,43 @@
+fruits =["Apple","Banana","Orange","Mango"]
+
+print(fruits)
+
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+print(fruits[3])
+
+fruits[1]="Pineapple"
+
+print(fruits)
+
+animals =["Dog","Cat","Cow","Goat"]
+animals[2]= "Lion"
+print(animals)
+
+
+fruits=["Apple","Banana","Orange"]
+fruits.append("Mango")
+
+print(fruits)
+
+
+fruits=["Apple","Banana","Orange","Mango"]
+fruits.remove("Banana")
+
+print(fruits)
+
+
+
+fruits=["Apple","Banana","Orange","Mango"]
+
+print(len(fruits))
+
+
+
+fruits=["Apple","Banana","Orange","Mango"]
+
+for fruit in fruits:
+    print(fruit)
+    
+
