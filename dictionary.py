edc96464231daf2b2a3c8bb90c1df41ea2e679d4
brandoo419 <brandonkiprotich68@gmail.com>
2026-07-08 11:06:51 +0300

    Completed another -Python dictionary

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+student ={
+    "name":"Brandon",
+    "age": 21,
+    "course":"Information Technology",
+    "year": "Third",
+    "city": "Nairobi"
+}
+
+print(student)
+
+print(student["name"])
+print(student["age"])
+print(student["course"])
+print(student["year"])
+print(student["city"])
