b31a8b3c5ff287f6d88c58abf7ac77c0dba412cc
brandoo419 <brandonkiprotich68@gmail.com>
2026-07-09 10:31:54 +0300

    Day 6 complete

---
+++ profile.py
@@ -0,0 +1,12 @@
+name = input("Enter your name:")
+
+print("Hello,",name)
+print("Your name has",len(name),"letters")
+print("Uppercase:", name.upper())
+print("Lowercase:", name.lower())
+print("First letter:", name[0])
+print("Last letter:", name[-1])
+
+print("First three letters:", name[:3])
+print("Last three letters:", name[-3:])
+
---
+++ strings.py
@@ -0,0 +1,22 @@
+name = "Brandon"
+ 
+print(name)
+
+print(name.upper())
+print(name.lower())
+
+print(len(name))
+
+sentence ="I love Python"
+
+print(sentence.replace("Python","Linux"))
+
+word= "Networking"
+
+print(word[0])
+print(word[1])
+print(word[2])
+print(word[-1])
+
+print(word[0:4])
+print(word[4:10])
